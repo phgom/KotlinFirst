702280d72449d75ffbdf8e35d8cc9e8f76a5641a
phgom <62889492+phgom@users.noreply.github.com>
2020-09-28 00:31:17 +0900

    add comment

--- app/src/main/java/com/example/kotlinfirst/Sample.kt
+++ app/src/main/java/com/example/kotlinfirst/Sample.kt
@@ -6,10 +6,14 @@ fun main() {
     println("Hello World ${c}")
 }
 
+
+// 리턴값이 없을때는 Unit이나 안붙여도 됨
 fun helloWorld() : Unit {
     println("First Line Print")
 }
 
+
+//var는 variable / val은 value로 val은 최초에 할당된 값을 변경할 수 없음
 fun helloWorld2(b : Int) : Int {
     val a : Int = 10
     return a+b
